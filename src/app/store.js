6bb42a76721c4f7b019d25855701ff668bcf46f9
Eric Varela <eric_varela@mac.com>
2020-09-15 18:52:31 -0400

    Added Logging Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import logger from 'redux-logger';
 import counterReducer from '../features/counter/counterSlice';
 import pokemonReducer from '../features/PokemonLoader/pokemon.slice';
+
+const middleware = [...getDefaultMiddleware(), logger];
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     pokemon: pokemonReducer,
   },
+  middleware,
 });
